98927953f741f78e301253774ac321948145dfb9
JasonVolkoff <kashijason@gmail.com>
2021-04-20 02:28:35 -0700

    Added methods to index and process money paths within views.py

--- ninja_gold_project/gold_app/views.py
+++ ninja_gold_project/gold_app/views.py
@@ -2,8 +2,23 @@ from django.shortcuts import render, redirect
 
 
 def index(request):
-    return
+    if "gold" not in request.session:
+        request.session["gold"] = 0
+
+    return render(request, "index.html")
 
 
 def process_money(request):
-    return
+    # check if info is posted
+    if request.method == "POST":
+        # convert to simple variables to ease of use
+        totalGold = request.session["gold"]
+        location = request.session["location"]
+        # check where we're at
+        if location == "farm":
+            # Earns 10-20g
+            goldThisTurn = round(random.random() * 10 + 10)
+            # add total with session gold
+            totalGold += goldThisTurn
+            request.session["gold"] = totalGold
+    return redirect("/")
